0eecdcc7821bcfd9743863e07dc944df96f164d8
Turovets Yevhen <y.turovets@runbyit.com>
2023-06-25 21:19:07 +0300

    feat: "Created a profile page"

--- src/app/portfolio/[category]/page.jsx
+++ src/app/portfolio/[category]/page.jsx
@@ -1,10 +1,57 @@
 import React from 'react';
 import styles from './page.module.css';
+import Button from "@/components/Button/Button";
+import Image from "next/image";
 
-const Category = () => {
+const Category = ({ params }) => {
     return (
         <div className={styles.container}>
-            Category
+            <h1 className={styles.catTitle}>{params.category}</h1>
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.description}>Description</p>
+                    <Button text="See more" url="#" />
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image
+                        className={styles.image}
+                        src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+                        alt=""
+                        fill={true}
+                    />
+                </div>
+            </div>
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.description}>Description</p>
+                    <Button text="See more" url="#" />
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image
+                        className={styles.image}
+                        src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+                        alt=""
+                        fill={true}
+                    />
+                </div>
+            </div>
+            <div className={styles.item}>
+                <div className={styles.content}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.description}>Description</p>
+                    <Button text="See more" url="#" />
+                </div>
+                <div className={styles.imgContainer}>
+                    <Image
+                        className={styles.image}
+                        src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+                        alt=""
+                        fill={true}
+                    />
+                </div>
+            </div>
         </div>
     );
 };
