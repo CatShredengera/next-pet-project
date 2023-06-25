cc5c7afbc64d0885ca47880777887b9c1b792299
Turovets Yevhen <y.turovets@runbyit.com>
2023-06-25 21:27:53 +0300

    feat: "Created a blog page"

--- src/app/blog/[id]/page.jsx
+++ src/app/blog/[id]/page.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 import styles from './page.module.css';
+import Image from "next/image";
 
 const BlogPost = () => {
     return (
         <div className={styles.container}>
-            BlogPost
+            <div className={styles.top}>
+                <div className={styles.info}>
+                    <h1 className={styles.title}>Test</h1>
+                    <p className={styles.desc}>
+                        Description
+                    </p>
+                    <div className={styles.author}>
+                        <Image
+                            src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+                            alt=""
+                            width={40}
+                            height={40}
+                            className={styles.avatar}
+                        />
+                        <span className={styles.username}>Test username</span>
+                    </div>
+                </div>
+                <div className={styles.imageContainer}>
+                    <Image
+                        src="https://images.pexels.com/photos/3130810/pexels-photo-3130810.jpeg"
+                        alt=""
+                        fill={true}
+                        className={styles.image}
+                    />
+                </div>
+            </div>
+            <div className={styles.content}>
+                <p className={styles.text}>
+                    Test Content
+                </p>
+            </div>
         </div>
     );
 };
